095ba751f981ea9c1a6f13e3c9dc29015638a968
钟磊 <zl_0110@outlook.com>
2024-09-14 13:38:28 +0800

    chore: update build process, update exports

--- packages/lib/vite.config.ts
+++ packages/lib/vite.config.ts
@@ -12,12 +12,17 @@ export default defineConfig({
         lib: {
             entry: "./src/main.ts",
             name: "VueSmoothDnDNext",
-            fileName: "vue-smooth-dnd-next",
+            fileName: (format) => `vue-smooth-dnd-next.${format}.js`,
             formats: ['es', 'cjs', 'umd'],
-
         },
         rollupOptions: {
             external: ["vue", "smooth-dnd-next"],
+            output: {
+                globals: {
+                  vue: 'Vue',
+                  'smooth-dnd-next': 'SmoothDndNext'
+                }
+            } 
         },
     },
 });
